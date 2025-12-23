33a7fef26b113709d8d1e2cac271e6c067efbac0
michael-otieno <otieno.oduor.michael@gmail.com>
2025-12-23 06:41:35 +0300

    updates

---
+++ project-2/versions.tf
@@ -0,0 +1,37 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.31"
+     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.3"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-on-aws-eks-a1"
+    key    = "dev/ebs-storage/terraform.tfstate"
+    region = "us-east-1" 
+
+    # For State Locking
+    dynamodb_table = "dev-ebs-storage"    
+  }     
+}
+
+# Terraform AWS Provider Block
+provider "aws" {
+  region = var.aws_region
+}
+
+# Terraform HTTP Provider Block
+provider "http" {
+  # Configuration options
+}
